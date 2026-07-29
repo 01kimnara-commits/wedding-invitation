ae54ae3aba7d5c2117f2567e422061212d9a1636
01kimnara-commits <01kim.nara@gmail.com>
2026-07-29 17:01:45 +0000

    feat: add light box

--- script.js
+++ script.js
@@ -80,3 +80,30 @@ function revealSection(){
 window.addEventListener("scroll", revealSection);
 
 revealSection();
+// ==========================
+// LIGHTBOX GALLERY
+// ==========================
+
+const galleryImages = document.querySelectorAll(".gallery-grid img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+galleryImages.forEach(img=>{
+
+    img.addEventListener("click",()=>{
+
+        lightbox.style.display="flex";
+
+        lightboxImg.src=img.src;
+
+    });
+
+});
+
+lightbox.addEventListener("click",()=>{
+
+    lightbox.style.display="none";
+
+});
