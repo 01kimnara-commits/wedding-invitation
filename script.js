4206c560b77d29ac961ec8679d82de5b7d5a8827
01kimnara-commits <01kim.nara@gmail.com>
2026-08-04 13:13:40 +0000

    feat: new design

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ document.getElementById("openInvitation").addEventListener("click", function () 
     alert("Selamat datang di Undangan Pernikahan Regi & Ratu 💕");
 
 });
+// ==========================
+// COUNTDOWN
+// ==========================
+
+const weddingDate = new Date("2026-12-20T08:00:00").getTime();
+
+setInterval(function(){
+
+    const now = new Date().getTime();
+
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000*60*60*24));
+
+    const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+
+    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+
+    const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+
+    document.getElementById("hours").innerHTML = hours;
+
+    document.getElementById("minutes").innerHTML = minutes;
+
+    document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
